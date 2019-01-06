4b96d23340d123631c3da0bdcb2e7989588ed9fc
sanadsa <sanad_s96@hotmail.com>
2019-01-06 16:59:24 +0200

    doc

--- SocialMedia/Social.Common/Models/User.cs
+++ SocialMedia/Social.Common/Models/User.cs
@@ -7,18 +7,16 @@ using System.Threading.Tasks;
 
 namespace Social.Common.Models
 {
+    /// <summary>
+    /// class for user in the social network app
+    /// </summary>
     public class User
     {
         public string Username { get; set; }
-
         public string Email { get; set; }
-
         public string Token { get; set; }
-
         public List<string> FollowersIds { get; set; }
-
         public List<string> Following { get; set; }
-
         public List<string> BlockedIds { get; set; }
     }
 }
